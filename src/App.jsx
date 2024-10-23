49bb7dea820311fd0ccea6b95564034c340be693
RohaanUmair <m.rohaanumair@gmail.com>
2024-10-23 07:43:40 +0500

    improvements

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,38 @@ import { onAuthStateChanged } from 'firebase/auth';
 import Swal from 'sweetalert2';
 
 function App() {
+ 
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
+        <AppContent />
+      </BrowserRouter>
+
+      {/* <BrowserRouter>
+
+        <Header />
+        <ScrollToTop />
+
+
+        
+        <Footer />
+        
+
+        <Routes>
+        </Routes>
+
+      </BrowserRouter> */}
+    </>
+  )
+}
+
+
+
+const AppContent = () => {
+
   const [cartProducts, setCartProducts] = useState([]);
   const [total, setTotal] = useState(0);
 
@@ -64,32 +96,30 @@ function App() {
 
     return () => unsubscribe();
   }, []);
+  const location = useLocation();
 
+  const isAuthRoute = location.pathname === '/login' || location.pathname === '/signup';
 
   return (
     <>
-      <BrowserRouter>
+      {!isAuthRoute && <Header />}
 
-        <Header />
-        <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<HomePage handleAddToCart={handleAddToCart} />} />
+        <Route path='/shop' element={<ShopPage handleAddToCart={handleAddToCart} />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
+        <Route path='/cart' element={<CartPage cartProductsArray={cartProducts} total={total} />} />
+        <Route path='/product/:id' element={<SingleProduct handleAddToCart={handleAddToCart} cartProductsArray={cartProducts} total={total} />} />
+        <Route path='/signedin' element={<SignedInPage handleSignOut={handleSignOutBtn} />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+      </Routes>
 
-        <Routes>
-          <Route path='/' element={<HomePage handleAddToCart={handleAddToCart} />} />
-          <Route path='/shop' element={<ShopPage handleAddToCart={handleAddToCart} />} />
-          <Route path='/contact' element={<ContactPage />} />
-          <Route path='/checkout' element={<CheckoutPage />} />
-          <Route path='/cart' element={<CartPage cartProductsArray={cartProducts} total={total} />} />
-          <Route path='/product/:id' element={<SingleProduct handleAddToCart={handleAddToCart} cartProductsArray={cartProducts} total={total} />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignupPage />} />
-          <Route path='/signedin' element={<SignedInPage handleSignOut={handleSignOutBtn} />} />
-        </Routes>
-
-        <Footer />
-
-      </BrowserRouter>
+      {!isAuthRoute && <Footer />}
     </>
   )
+
 }
 
-export default App;
+export default App; 
