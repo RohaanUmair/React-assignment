217d54dd8d3e5f578dd51b919aa1aa07d1ba3dfd
RohaanUmair <m.rohaanumair@gmail.com>
2024-10-29 23:04:24 +0500

    responsiveness

--- src/pages/SignedInPage.jsx
+++ src/pages/SignedInPage.jsx
@@ -1,4 +1,4 @@
-import styles from '../components/signedIn.module.css';
+import styles from "../components/signedIn.module.css";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
@@ -17,20 +17,6 @@ function SignedInPage({ handleSignOut }) {
         });
     }, [navigate]);
 
-    const signOutBtnStyles = {
-        backgroundColor: "white",
-        marginLeft: 10,
-        color: "#b88e2f",
-        border: "1px solid #b88e2f",
-        fontWeight: 500,
-        width: 200,
-        height: 50,
-        fontFamily: "Poppins",
-        fontSize: 19,
-        marginTop: 15,
-        cursor: 'pointer'
-    };
-
     return (
         <div
             style={{
@@ -43,11 +29,14 @@ function SignedInPage({ handleSignOut }) {
             }}
         >
             <div>
-                <h1 style={{ fontSize: 40 }}>You are Signed In.</h1>
-                <button onClick={() => navigate("/shop")} className={styles.gotoShopBtnStyles} >
+                <h1 className={styles.signedInHeading}>You are Signed In.</h1>
+                <button
+                    onClick={() => navigate("/shop")}
+                    className={styles.gotoShopBtnStyles}
+                >
                     Shop
                 </button>
-                <button onClick={handleSignOut} className={styles.signOutBtnStyles} >
+                <button onClick={handleSignOut} className={styles.signOutBtnStyles}>
                     Sign Out
                 </button>
             </div>
